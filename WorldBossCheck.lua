b0ce6135d89198b180b8b12b526e813d34381b1a
CappeD <CappeD@users.noreply.github.com>
2025-08-19 00:37:10 +0200

    Added bosses for all phases of MoP. Only bosses from current phase are listed by default. ("/wbc all" to override default behaviour)

--- WorldBossCheck.lua
+++ WorldBossCheck.lua
@@ -1,21 +1,31 @@
-local function CheckWorldBossStatus()
+local function CheckWorldBossStatus(input)
+    local currentPhase = 1
+
     local worldBosses = {
-        {boss = "Sha of Anger", qid = 32099},
-        {boss = "Galleon", qid = 32098}
+        {boss = "Sha of Anger", qid = 32099, phase = 1},
+        {boss = "Galleon", qid = 32098, phase = 1},
+        {boss = "Nalak", qid = 32518, phase = 3},
+        {boss = "Oondasta", qid = 32519, phase = 3},
+        {boss = "Celestials", qid = 33117, phase = 5},
+        {boss = "Ordos", qid = 33118, phase = 5}
     }
+
+    local showAll = input and input:lower() == "all"
 
     print("WorldBossCheck:")
     for _, b in ipairs(worldBosses) do
-        local isCompleted = C_QuestLog.IsQuestFlaggedCompleted(b.qid)
-        local status
+        if showAll or b.phase <= currentPhase then
+            local isCompleted = C_QuestLog.IsQuestFlaggedCompleted(b.qid)
+            local status
 
-        if isCompleted then
-            status = "|cff00ff00killed|r"
-        else
-            status = "|cffffff00available|r"
+            if isCompleted then
+               status = "|cff00ff00killed|r"
+            else
+              status = "|cffffff00available|r"
+            end
+
+            print(string.format("%s - %s", b.boss, status))
         end
-
-        print(string.format("%s - %s", b.boss, status))
     end
 end
 
